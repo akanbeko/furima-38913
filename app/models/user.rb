fd2615ac8f1bade5295dd678bd6467156f27023c
akanbeko <124563135+akanbeko@users.noreply.github.com>
2023-03-07 11:11:16 +0900

    userモデルバリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,25 +4,21 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nickname
-         validates :email, uniqueness: true
-         
-         with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
-          validates :first_name
-          validates :last_name
-         end
-         
-         with_options presence: true, format: { with: /\A[ァ-ヶー－]+\z/ , message: '全角カタカナを使用してください' } do
-          validates :last_name_kana
-          validates :first_name_kana
-         end
-         
-         validates :birthday
-         
-         
          has_many :items
          has_many :buys
 
-      PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-      validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
-end
+         with_options presence: true do
+          validates :nickname
+          validates :email,    uniqueness: true
+          validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+          validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
+          validates :last_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+          validates :first_name_kana, format: { with: /\A[ァ-ヶー－]+\z/ }
+          validates :birthday
+
+          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+          validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+        end
+
+
+ end
