31907ae19016d871d24d87c9789826b947f9e7e3
akanbeko <124563135+akanbeko@users.noreply.github.com>
2023-03-07 11:48:59 +0900

    appuricationコントローラーエラーの解消

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,10 @@ class ApplicationController < ActionController::Base
       username == 'admin' && password == '2222'
       # username == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"]
     end
+  end
     def configure_permitted_parameters
       devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birthday])
     end
-  end
-end
+
+
+  end   
