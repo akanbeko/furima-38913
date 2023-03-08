6084b5226b757d594e3b828043be8abd2fb791ef
akanbeko <124563135+akanbeko@users.noreply.github.com>
2023-03-08 16:05:13 +0900

    javascript 販売手数料の計算

--- Gemfile
+++ Gemfile
@@ -62,6 +62,9 @@ group :production do
 end
 
 gem 'devise'
+
 gem 'rspec-rails', '~> 4.0.0'
-  gem 'factory_bot_rails'
-  gem 'faker'
+gem 'factory_bot_rails'
+gem 'faker'
+
+gem 'active_hash'
---
+++ app/models/item.rb
@@ -0,0 +1,26 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  
+  belongs_to :user
+
+  belongs_to :category
+  belongs_to :condition
+  belongs_to :charges
+  belongs_to :area
+  belongs_to :days
+
+  has_one_attached :image
+
+  with_options presence: true do
+  validates :user_id
+  validates :image
+  validates :name
+  validates :text
+  validates :category_id, numericality: { other_than: 1 } 
+  validates :condition_id, numericality: { other_than: 1 } 
+  validates :charges_id, numericality: { other_than: 1 } 
+  validates :area_id, numericality: { other_than: 1 } 
+  validates :days_id, numericality: { other_than: 1 } 
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  end
+end
