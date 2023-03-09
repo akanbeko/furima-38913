5856c161e0a81bead72038a396838657b111f4be
akanbeko <124563135+akanbeko@users.noreply.github.com>
2023-03-09 17:50:02 +0900

    金額　小数点以下は保存しない

--- app/models/item.rb
+++ app/models/item.rb
@@ -20,6 +20,6 @@ class Item < ApplicationRecord
   validates :charge_id, numericality: { other_than: 1, message: "can't be blank"}  
   validates :area_id, numericality: { other_than: 1 , message: "can't be blank"} 
   validates :days_id, numericality: { other_than: 1 , message: "can't be blank"}  
-  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+  validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999, only_integer: true }
   end
 end
